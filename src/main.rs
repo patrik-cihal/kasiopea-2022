c31651444dc3221922ed49b66484b88d95d08adb
Patrik Cihal <patrik.cihal1234@gmail.com>
2023-02-07 17:20:26 +0100

    Finals template

---
+++ src/main.rs
@@ -0,0 +1,55 @@
+use std::io::Write;
+
+mod a;
+mod b;
+mod c;
+mod d;
+mod e;
+mod f;
+
+type Input = token_read::TokenReader<std::io::StdinLock<'static>>;
+
+pub static mut OUTPUT: Option<std::io::BufWriter<std::io::Stdout>> = None;
+pub static mut INPUT: Option<Input> = None;
+
+fn main() {
+    let (t, ): (usize, ) = scanln();
+    for _ in 0..t {
+        a::solve();
+    }
+    flush();
+}
+
+#[macro_export]
+macro_rules! out {
+    ($($arg:tt)*) => {{
+        let output = unsafe {
+            OUTPUT.get_or_insert_with(|| std::io::BufWriter::with_capacity(1024*1024, std::io::stdout()))
+        };
+        write!(output, $($arg)*).unwrap();
+    }};
+}
+
+#[macro_export]
+macro_rules! outln {
+    ($($arg:tt)*) => {{
+        let output = unsafe {
+            OUTPUT.get_or_insert_with(|| std::io::BufWriter::with_capacity(1024*1024, std::io::stdout()))
+        };
+        writeln!(output, $($arg)*).unwrap();
+    }};
+}
+
+fn flush() {
+    let output = unsafe {
+        OUTPUT.get_or_insert_with(|| std::io::BufWriter::with_capacity(1024*1024, std::io::stdout()))
+    };
+    output.flush().unwrap();
+}
+
+fn scanln<T: token_read::FromTokens>() -> T where <T as token_read::FromTokens>::Error: std::fmt::Debug {
+    let input = unsafe {
+        INPUT.get_or_insert_with(|| Input::new(std::io::stdin().lock()))
+    };
+    input.line().unwrap()
+}
